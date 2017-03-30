20b31d5f8a9c46bc8b483e01bd929d592344ec20
Aleksi Savijoki <aleksi.savijoki@student.tut.fi>
2017-03-30 20:42:15 +0300

    Added models for recipes and mealplans. Added to django admin

--- food/admin.py
+++ food/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from food.models import Recipe, MealPlan
 
-# Register your models here.
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
+
+admin.site.register(Recipe, RecipeAdmin)
--- food/models.py
+++ food/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """
+    Class for defining recipes
+    """
+
+    title = models.CharField(
+        max_length=128,
+        primary_key=True,
+    )
+    time = models.PositiveSmallIntegerField(
+        "Cooking time in minutes",
+    )
+    imageURL = models.URLField(
+        "URL to recipe's image",
+        max_length=128
+    )
+    summary = models.CharField(
+        "Summary of the recipe",
+        max_length=256,
+    )
+
+
+class MealPlan(models.Model):
+    """
+    A collection of recipes. The amount of recipes
+    is defined by period.
+    """
+
+    DAY = 1
+    WEEK = 7
+    PERIOD_CHOICES = (
+        (DAY, 'Day'),
+        (WEEK, 'Week'),
+    )
+    period = models.CharField(
+        "Meal plan's period",
+        max_length=4,
+        choices=PERIOD_CHOICES,
+        default=DAY,
+    )
+    recipes = models.ManyToManyField(
+        Recipe,
+    )
